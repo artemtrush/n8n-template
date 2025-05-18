b675356076d6a6daa56f5bf4591a1ac709c9eb05
artemtrush <atrush.main@gmail.com>
2025-05-18 17:05:44 +0300

    Update ngingx config

--- scripts/init-certbot.sh
+++ scripts/init-certbot.sh
@@ -3,7 +3,4 @@ set -e
 
 source .env
 
-docker compose run --rm certbot certonly \
-  --webroot -w /var/www/certbot \
-  -d ${DOMAIN_NAME} \
-  --email ${ADMIN_EMAIL} --agree-tos --no-eff-email
+docker compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot -d ${DOMAIN_NAME} --email ${ADMIN_EMAIL} --agree-tos --no-eff-email" certbot
